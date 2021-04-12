8dba18ac2d9ec090d5089bf56c0b8bad4806474a
Bernardo Meurer <bernardo@meurer.org>
2021-04-12 12:08:07 -0700

    initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:figsoda/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fenixPkgs = fenix.packages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (fenixPkgs.complete.withComponents [
+              "cargo"
+              "clippy-preview"
+              "rust-src"
+              "rust-std"
+              "rustc"
+              "rustfmt-preview"
+            ])
+            fenixPkgs.rust-analyzer
+
+            cargo-edit
+            nixpkgs-fmt
+            exercism
+          ];
+        };
+      });
+}
