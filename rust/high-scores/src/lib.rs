a7247da161609978b175504539a948254c3e3cc7
Bernardo Meurer <bernardo@meurer.org>
2021-04-13 16:17:20 -0700

    rust/high-scores: complete

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().copied()
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().copied()
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut sorted = self.scores.to_owned();
+        sorted.sort_unstable_by(|a, b| a.cmp(b).reverse());
+        let top = sorted.len().min(3);
+        sorted[0..top].to_owned()
+    }
+}
