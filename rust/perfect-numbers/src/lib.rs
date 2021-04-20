5007a17edb77f4c2dd00e8b165bc720d470d51b6
Bernardo Meurer <bernardo@meurer.org>
2021-04-19 17:20:47 -0700

    rust/perfect-numbers: complete

---
+++ rust/perfect-numbers/src/lib.rs
@@ -0,0 +1,23 @@
+use std::cmp::Ordering;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Classification {
+    Abundant,
+    Perfect,
+    Deficient,
+}
+
+fn aliquot_sum(n: u64) -> u64 {
+    (1..n).filter(|i| n % i == 0).sum()
+}
+
+pub fn classify(num: u64) -> Option<Classification> {
+    match num {
+        0 => None,
+        n => match n.cmp(&aliquot_sum(n)) {
+            Ordering::Equal => Some(Classification::Perfect),
+            Ordering::Greater => Some(Classification::Deficient),
+            Ordering::Less => Some(Classification::Abundant),
+        },
+    }
+}
