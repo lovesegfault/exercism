dd2c46690926a524fd16fc15f56964bb27a2fcf7
Bernardo Meurer <bernardo@meurer.org>
2021-04-19 21:05:26 -0700

    rust/etl: complete

---
+++ rust/etl/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::BTreeMap;
+
+pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
+    h.iter()
+        .flat_map(|(score, letters)| {
+            letters
+                .iter()
+                .map(|l| l.to_ascii_lowercase())
+                .map(move |l| (l, *score))
+        })
+        .collect()
+}
