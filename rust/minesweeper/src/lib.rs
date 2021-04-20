8668b7190744cdbccb83100815e25853fdc01331
Bernardo Meurer <bernardo@meurer.org>
2021-04-20 14:38:25 -0700

    rust/minesweeper: complete

---
+++ rust/minesweeper/src/lib.rs
@@ -0,0 +1,33 @@
+fn count_at((x, y): (usize, usize), minefield: &[Vec<char>]) -> String {
+    let x_min = x.saturating_sub(1);
+    let y_min = y.saturating_sub(1);
+    let count = (x_min..(x + 2))
+        .flat_map(|x| (y_min..(y + 2)).map(move |y| (x, y)))
+        .filter_map(|(x, y)| minefield.get(x).and_then(|r| r.get(y)))
+        .filter(|&&c| c == '*')
+        .count();
+    match count {
+        0 => " ".to_string(),
+        x => x.to_string(),
+    }
+}
+
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    // I simply refuse to use &[&str], it's a horrible choice for type and just causes headaches
+    let minefield: Vec<Vec<char>> = minefield.iter().map(|r| r.chars().collect()).collect();
+
+    minefield
+        .iter()
+        .enumerate()
+        .map(|(i, r)| {
+            r.iter()
+                .enumerate()
+                .map(move |(j, _)| (i, j))
+                .map(|(i, j)| match minefield[i][j] {
+                    '*' => "*".to_string(),
+                    _ => count_at((i, j), &minefield),
+                })
+                .collect()
+        })
+        .collect()
+}
