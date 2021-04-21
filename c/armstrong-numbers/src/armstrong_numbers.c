369074e6de9215296278e4b8e33228267879a1fa
Bernardo Meurer <bernardo@meurer.org>
2021-04-21 13:18:13 -0700

    c/armstrong-numbers: mentor feedback

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,27 +1,14 @@
 #include "armstrong_numbers.h"
 #include <math.h>
+#include <stdlib.h>
 
 bool is_armstrong_number(int candidate) {
-    int acc = candidate;
-    int num_len = 0;
-    while (acc > 0) {
-        num_len++;
-        acc /= 10;
-    }
-
-    int digits[num_len];
-    acc = candidate;
-    num_len = 0;
-    while (acc > 0) {
-        int current_digit = acc % 10;
-        digits[num_len] = current_digit;
-        num_len++;
-        acc /= 10;
-    }
+    int num_len = floor(log10((double) abs(candidate))) + 1;
 
     int sum = 0;
-    for (int i = 0; i < num_len; ++i) {
-        sum += pow(digits[i], num_len);
+    for (int i = candidate; i > 0; i /= 10) {
+        int current_digit = i % 10;
+        sum += pow(current_digit, num_len);
     }
 
     return (sum == candidate);
