e738fcf37850f65598c043316b071736b2a3d7eb
Bernardo Meurer <bernardo@meurer.org>
2021-04-21 15:53:57 -0700

    c/armstrong-numbers: work around 0 issue

--- c/armstrong-numbers/src/armstrong_numbers.c
+++ c/armstrong-numbers/src/armstrong_numbers.c
@@ -1,9 +1,14 @@
 #include "armstrong_numbers.h"
 #include <math.h>
-#include <stdlib.h>
 
 bool is_armstrong_number(int candidate) {
-    int num_len = floor(log10((double) abs(candidate))) + 1;
+    if (candidate < 0) {
+        return false;
+    } else if (candidate == 0) {
+        return true;
+    }
+
+    int num_len = log10(candidate) + 1;
 
     int sum = 0;
     for (int i = candidate; i > 0; i /= 10) {
