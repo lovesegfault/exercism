ab605402da3c23d36d1ebf9dc9865e9da06c9a62
Bernardo Meurer <bernardo@meurer.org>
2021-04-22 03:52:28 -0700

    haskell/acronym: complete

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -1,7 +1,12 @@
 module Acronym (abbreviate) where
 
-import qualified Data.Text as T
+import Data.Char (isSpace, isUpper)
 import Data.Text (Text)
+import qualified Data.Text as T
 
 abbreviate :: Text -> Text
-abbreviate xs =  T.split (\c -> c == ' ' || c == '-' || c== '_') xs
+abbreviate =
+  T.toUpper
+    . T.concat
+    . map (\w -> T.take 1 w <> (T.filter isUpper . T.dropWhile isUpper) w)
+    . T.split (\c -> isSpace c || c `elem` ['-', '_'])
