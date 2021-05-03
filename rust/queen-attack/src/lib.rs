a91e226122345a433c6dddd30c78cfd08413b144
Bernardo Meurer <bernardo@meurer.org>
2021-05-03 11:43:45 -0700

    rust/queen-attack: simplify

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -6,29 +6,10 @@ pub struct ChessPosition {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        if !((0..8).contains(&rank) && (0..8).contains(&file)) {
-            return None;
+        match (rank, file) {
+            (0..=7, 0..=7) => Some(Self { rank, file }),
+            _ => None,
         }
-        Some(Self { rank, file })
-    }
-
-    pub fn lines(&self) -> impl Iterator<Item = Self> + '_ {
-        let (rank, file) = (self.rank, self.file);
-        (0..8)
-            .flat_map(move |i| std::array::IntoIter::new([(rank, i), (i, file)]))
-            .filter_map(|(rank, file)| Self::new(rank, file))
-    }
-
-    pub fn diagonals(&self) -> impl Iterator<Item = Self> {
-        let (rank, file) = (self.rank, self.file);
-        (0..=rank)
-            .enumerate()
-            .chain((rank..8).enumerate())
-            .map(|(i, rank)| (i as i32, rank))
-            .flat_map(move |(i, rank)| {
-                std::array::IntoIter::new([(file - i, rank), (file + i, rank)])
-            })
-            .filter_map(|(rank, file)| Self::new(rank, file))
     }
 }
 
@@ -40,14 +21,9 @@ impl Queen {
         Self(position)
     }
 
-    fn attacked_positions(&self) -> impl Iterator<Item = ChessPosition> + '_ {
-        let lines = self.0.lines();
-        let diagonals = self.0.diagonals();
-        lines.chain(diagonals)
-    }
-
     pub fn can_attack(&self, other: &Queen) -> bool {
-        self.attacked_positions()
-            .any(|attack_position| attack_position == other.0)
+        let rank_delta = (self.0.rank - other.0.rank).abs();
+        let file_delta = (self.0.file - other.0.file).abs();
+        rank_delta == 0 || file_delta == 0 || rank_delta == file_delta
     }
 }
